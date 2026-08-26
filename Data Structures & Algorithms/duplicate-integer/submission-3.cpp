e321039037ea98f3c243c4571de9cc8099f4c086
johnnathanieI <117893352+johnnathanieI@users.noreply.github.com>
2026-08-26 15:01:32 -0600

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(std::vector<int>& nums) {
+        for (size_t i=0; i < nums.size(); i++) {
+            for (size_t j=i+1; j < nums.size(); j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
